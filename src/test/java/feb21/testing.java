77c8716b20bd56c5a95f8db25b7dbcbede367d57
Aruna <arunapaari@gmail.com>
2023-02-22 12:59:41 +0530

    2week

--- src/test/java/feb21/testing.java
+++ src/test/java/feb21/testing.java
@@ -1,5 +1,9 @@
 package feb21;
 
 public class testing {
-
+	public static void main (String []args) {
+System.out.println("Gitclass");
+System.out.println("piece of glass");
+System.out.println("sssss");
+}
 }
